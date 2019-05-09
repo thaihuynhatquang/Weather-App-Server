a4d26eb5d4dd9b4e0c87c542ecebc0fab0921b8f
Trần Quang Linh <tranquanglinh.pt@gmail.com>
2019-05-09 08:04:32 +0700

    API login, register

--- Route/main_route.js
+++ Route/main_route.js
@@ -1,5 +1,6 @@
 var jsonParser = require('body-parser').json();  // nhận json từ client
 var weather_route = require('./weather_route');
+var user_route= require('./user_route');
 
 module.exports = {
     route: function (app) {
@@ -8,5 +9,8 @@ module.exports = {
         app.get('/weather/current/:cityName',(req,res)=>weather_route.send_current_weather_by_cityName(req,res));  //ok
         app.get('/weather/detailfiveday/lat=:lat/lon=:lon',(req,res)=>weather_route.send_fiveday_weather_by_location(req,res));  //ok
         app.get('/weather/detailfiveday/:cityName',(req,res)=>weather_route.send_fiveday_weather_by_cityName(req,res));  //ok
+        app.post('/user/login',jsonParser,(req,res)=>user_route.login(req,res));
+        app.post('/user/register',jsonParser,(req,res)=>user_route.register(req,res));
+        app.post('/user/loginWithGoogle',jsonParser,(req,res)=>user_route.loginG(req,res));
     }
 }
